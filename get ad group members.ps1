dc3b998705084a57b73309ff5919a5a703cc932a
Nick Elwood <elwoodn@me.com>
2021-05-25 15:36:31 -0400

    removed second pipe select

--- get ad group members.ps1
+++ get ad group members.ps1
@@ -1,6 +1,1 @@
-﻿Get-ADgroupmember -identity “” | get-aduser -property displayname, mail | select name, displayname, samaccountname, mail 
-
-
-
-git config --global user.email "elwoodn@me.com"
-git config --global user.name "Nick Elwood"
+﻿Get-ADgroupmember -identity “” | get-aduser -property displayname, mail 
