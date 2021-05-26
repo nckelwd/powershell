34fe7c35f5a7ecf5087069a48805411ac8bb97a4
Nick Elwood <elwoodn@me.com>
2021-05-26 10:30:54 -0400

    updated scripts

--- check csv eeid info against ad.ps1
+++ check csv eeid info against ad.ps1
@@ -2,7 +2,7 @@
 
 foreach ($empid in $csv) {
     $e = $empid.empID 
-    get-aduser -filter {(employeeID -like $e)} -Properties employeeid, enabled | select name, employeeID, enabled | export-csv "C:\Scripts\employeeAudit.csv" -NoTypeInformation -append
+    get-aduser -filter {(employeeID -like $e)} -Properties employeeid, enabled | Select-Object name, employeeID, enabled | export-csv "C:\Scripts\employeeAudit.csv" -NoTypeInformation -append
     
 } 
  
--- get all ad groups and descriptions.ps1
+++ get all ad groups and descriptions.ps1
@@ -1,3 +1,18 @@
 ﻿Import-Module Activedirectory
 
-Get-ADGroup -filter * -properties *|select SAMAccountName, Description|Export-Csv -Path c:\Scripts\ADGroupList.csv
+$savePAth = "C:\Scripts\ADCounter.csv"
+
+$Groups = Get-ADGroup -filter {GroupCategory -eq 'security'} -Properties * | Select-Object SAMAccountName | Export-Csv $savePAth
+
+Import-Csv $savePAth
+$Results = @()
+
+foreach ($G in $Groups) {
+    $num = Get-ADGroupMember -Identity $G.SamAccountName
+    ##Write-Host "the group" $g.SamAccountName "has" $num.count "users"
+    ##$Export  | Add-Member -MemberType NoteProperty -Name $g -Value $num.count
+    $G | Add-Member -MemberType NoteProperty -Name "UserCount" -Value $num.count
+    $Results += $G
+}
+
+$Results | Export-Csv "C:\Scripts\ADCounter.csv" -NoTypeInformation -Append
