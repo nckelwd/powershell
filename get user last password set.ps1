928ab93d5526a3fa8fb934a9ed8ad9046fdfd733
Nick Elwood <elwoodn@me.com>
2022-04-26 08:56:16 -0400

    modified to allow start and end on cmd

--- get user last password set.ps1
+++ get user last password set.ps1
@@ -1,7 +1,36 @@
+
+function ADPWSearch {
+
 $namesearch = Read-Host 'What is the username?'
-
 $filter = 'sAMAccountName -eq $namesearch'
-$users = Get-ADUser -Filter $filter -Properties * |  select enabled, displayname, description, physicalDeliveryOfficeName, emailaddress, @{name ="pwdLastSet";` expression={[datetime]::FromFileTime($_.pwdLastSet)}}
+$users = Get-ADUser -Filter $filter -Properties * |  select enabled, displayname, description, physicalDeliveryOfficeName, emailaddress, "msDS-cloudExtensionAttribute5", @{name ="pwdLastSet";` expression={[datetime]::FromFileTime($_.pwdLastSet)}}
 
+if ($users) {
+    $users
+    ADPWRetry
+}
+else {
+    Write-Host $namesearch "does not exist."
+    ADPWRetry
+}
 
-$users
+}
+
+function ADPWRetry {
+
+$tryagain = Read-Host 'Search another user? Use Y or N to proceed.'
+
+if ($tryagain -eq "Y") {
+    ADPWSearch
+}
+elseif ($tryagain -eq "N") {
+    Write-host "Quit searching."
+    exit
+}
+else {
+    Write-host "Invaild response. Quit searching."
+    exit
+}
+}
+
+ADPWSearch
