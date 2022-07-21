59840dda214cc1bdb9c70ee88e8cc716a02c04d2
Nick Elwood <elwoodn@me.com>
2022-07-21 10:23:49 -0400

    update

--- exchangeonline - remove mailbox automapping.ps1
+++ exchangeonline - remove mailbox automapping.ps1
@@ -4,7 +4,23 @@ Connect-ExchangeOnline
 $user = "bernardo.hobrecht@aprenergy.com"
 
 #enter in all the delegate email address you want to remove from the user
-$delegateEmails = ("ivan.achaga@aprenergy.com")
+$delegateEmails = (
+"Javier.Berge@aprenergy.com",
+"Ariel.Bidart@aprenergy.com",
+"Pedro.Brandan@aprenergy.com",
+"Cristian.DelRio@aprenergy.com",
+"Nazareno.Dulcce@aprenergy.com",
+"Damian.Marinos@aprenergy.com",
+"Facundo.Miraball@aprenergy.com",
+"Jose.Montiel@aprenergy.com",
+"Yolin.Moya@aprenergy.com",
+"Diego.Quinteros@aprenergy.com",
+"Luis.Rios@aprenergy.com",
+"Marcelo.Rizzardi@aprenergy.com",
+"Federico.Salinas@aprenergy.com",
+"Jose.Santangelo@aprenergy.com",
+"Lucas.Torrozzi@aprenergy.com",
+"Matias.Venini@aprenergy.com")
 
 foreach ($d in $delegateEmails) {
 Add-MailboxPermission -Identity $d -User $user -AccessRights FullAccess -InheritanceType All -Automapping $false
